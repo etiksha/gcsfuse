cecdf82b7cf1ed536b46400683d97a9764197d9f
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-16 14:38:03 +0530

    Update build.sh

--- perfmetrics/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/continuous_test/gcp_ubuntu/build.sh
@@ -6,7 +6,7 @@ sudo apt-get install fio -y
 echo Installing gcsfuse
 curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v0.41.1/gcsfuse_0.41.1_amd64.deb
 sudo dpkg --install gcsfuse_0.41.1_amd64.deb
-cd "${ARTIFACTS_DIR}/github/gcsfuse/perfmetrics"
+cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics"
 echo Mounting gcs bucket
 mkdir gcs
 gcsfuse --implicit-dirs --max-conns-per-host 100 --disable-http2 gcs-fuse-dashboard-fio gcs 
