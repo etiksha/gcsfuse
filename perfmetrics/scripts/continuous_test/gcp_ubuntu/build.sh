105b8222ce0721bf50b8e744512494d7067b7a3f
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-17 13:20:07 +0530

    Update build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -1,20 +1,20 @@
 #i!/bin/bash
 set -e
 sudo apt-get update
-echo Installing fio
-sudo apt-get install fio -y
-echo Installing gcsfuse
-GCSFUSE_VERSION=0.41.1
-curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$GCSFUSE_VERSION/gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
-sudo dpkg --install gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
+# echo Installing fio
+# sudo apt-get install fio -y
+# echo Installing gcsfuse
+# GCSFUSE_VERSION=0.41.1
+# curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$GCSFUSE_VERSION/gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
+# sudo dpkg --install gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 echo Mounting gcs bucket
 mkdir -p gcs
 GCSFUSE_FLAGS="--implicit-dirs --max-conns-per-host 100 --disable-http2"
 BUCKET_NAME=gcs-fuse-dashboard-fio
 MOUNT_POINT=gcs
-script logfile
-gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
+script -c "gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT" logfile
+#gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
 echo printing logfile
 cat logfile
 chmod +x build.sh
