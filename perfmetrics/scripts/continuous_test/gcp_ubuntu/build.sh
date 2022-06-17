1be2fa0621ad7f87ba7cdd63af2c88dff2a3c354
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-17 12:45:30 +0530

    Update build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -1,6 +1,6 @@
 #i!/bin/bash
 set -e
-./build.sh | tee -a logfile
+scripts -a logfile
 sudo apt-get update
 echo Installing fio
 sudo apt-get install fio -y
@@ -8,7 +8,7 @@ echo Installing gcsfuse
 GCSFUSE_VERSION=0.41.1
 curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$GCSFUSE_VERSION/gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
 sudo dpkg --install gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
-pwd 
+pwd
 ls
 cd "${KOKORO_ARTIFACTS_DIR}/github/gcsfuse/perfmetrics/scripts"
 echo Mounting gcs bucket
@@ -18,8 +18,8 @@ BUCKET_NAME=gcs-fuse-dashboard-fio
 MOUNT_POINT=gcs
 gcsfuse $GCSFUSE_FLAGS $BUCKET_NAME $MOUNT_POINT
 chmod +x build.sh
-./build.sh | tee -a ...../logfile
-cat ....../logfile
+./build.sh -a tmpfs/src/logfile
+cat tmpfs/src/logfile
 
 
 # #i!/bin/bash
