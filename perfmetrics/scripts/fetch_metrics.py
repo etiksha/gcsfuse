2199d7af4e468f0237dc1768f354773acfc7dc02
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-21 15:58:05 +0530

    Update fetch_metrics.py

--- perfmetrics/scripts/fetch_metrics.py
+++ perfmetrics/scripts/fetch_metrics.py
@@ -12,30 +12,7 @@ END_TIME = 'end_time'
 INSTANCE = socket.gethostname()
 PERIOD = 120
 
-async def fun():
-  argv = sys.argv
-  if len(argv) != 2:
-    raise TypeError('Incorrect number of arguments.\n'
-                    'Usage: '
-                    'python3 fetch_metrics.py <fio output json filepath>')
-
-  fio_metrics_obj = fio_metrics.FioMetrics()
-  temp = await fio_metrics_obj.get_metrics(argv[1], True)
-  print(time.time())
-  time.sleep(250)
-  print(time.time())
-  vm_metrics_obj = vm_metrics.VmMetrics()
-  for job in temp:
-    start_time_sec = job[START_TIME]
-    end_time_sec = job[END_TIME]
-    print(start_time_sec)
-    print(end_time_sec)
-    vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(start_time_sec,
-                                                     end_time_sec, INSTANCE,
-                                                     PERIOD)
-
-if __name__ == '__main__':
-  asyncio.run(fun())
+# async def fun():
 #   argv = sys.argv
 #   if len(argv) != 2:
 #     raise TypeError('Incorrect number of arguments.\n'
@@ -46,6 +23,7 @@ if __name__ == '__main__':
 #   temp = await fio_metrics_obj.get_metrics(argv[1], True)
 #   print(time.time())
 #   time.sleep(250)
+#   print(time.time())
 #   vm_metrics_obj = vm_metrics.VmMetrics()
 #   for job in temp:
 #     start_time_sec = job[START_TIME]
@@ -55,4 +33,29 @@ if __name__ == '__main__':
 #     vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(start_time_sec,
 #                                                      end_time_sec, INSTANCE,
 #                                                      PERIOD)
+
+if __name__ == '__main__':
+#   asyncio.run(fun())
+  argv = sys.argv
+  if len(argv) != 2:
+    raise TypeError('Incorrect number of arguments.\n'
+                    'Usage: '
+                    'python3 fetch_metrics.py <fio output json filepath>')
+
+  fio_metrics_obj = fio_metrics.FioMetrics()
+  temp = fio_metrics_obj.get_metrics(argv[1], True)
+  print(time.time())
+  time.sleep(250)
+  print(time.time())
+  vm_metrics_obj = vm_metrics.VmMetrics()
+  for job in temp:
+    start_time_sec = job[START_TIME]
+    end_time_sec = job[END_TIME]
+    print("job")
+    print(time.time())
+    print(start_time_sec)
+    print(end_time_sec)
+    vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(start_time_sec,
+                                                     end_time_sec, INSTANCE,
+                                                     PERIOD)
  
