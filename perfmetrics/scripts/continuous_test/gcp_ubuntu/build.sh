feacea7d6435069d7c2f9e45395883c4bee245a2
etiksha <107264895+etiksha@users.noreply.github.com>
2022-06-28 22:57:12 +0530

    Update build.sh

--- perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
+++ perfmetrics/scripts/continuous_test/gcp_ubuntu/build.sh
@@ -3,7 +3,7 @@ set -e
 sudo apt-get update
 echo Installing fio
 sudo apt-get install fio -y
-echo Installing gcsfuse a	q
+echo Installing gcsfuse 
 GCSFUSE_VERSION=0.41.1
 curl -L -O https://github.com/GoogleCloudPlatform/gcsfuse/releases/download/v$GCSFUSE_VERSION/gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
 sudo dpkg --install gcsfuse_"$GCSFUSE_VERSION"_amd64.deb
