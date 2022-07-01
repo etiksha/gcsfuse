92c47d28cba51821be8b1516ed455152ee2190db
etiksha <107264895+etiksha@users.noreply.github.com>
2022-07-01 10:34:20 +0530

    Update fetch_metrics.py

--- perfmetrics/scripts/fetch_metrics.py
+++ perfmetrics/scripts/fetch_metrics.py
@@ -8,6 +8,7 @@ from vm_metrics import vm_metrics
 
 START_TIME = 'start_time'
 END_TIME = 'end_time'
+RW = 'rw'
 INSTANCE = socket.gethostname()
 PERIOD = 120
 
@@ -21,16 +22,23 @@ if __name__ == '__main__':
   fio_metrics_obj = fio_metrics.FioMetrics()
   print('Getting fio metrics...')
   temp = fio_metrics_obj.get_metrics(argv[1])
+
   print('Waiting for 250 seconds for metrics to be updated on VM...')
+  # It takes up to 240 seconds for sampled data to be visible on the VM metrics graph
+  # So, waiting for 250 seconds to ensure the returned metrics are not empty
   time.sleep(250)
+
   vm_metrics_obj = vm_metrics.VmMetrics()
+  # Getting VM metrics for every job
   for ind, job in enumerate(temp):
     start_time_sec = job[START_TIME]
     end_time_sec = job[END_TIME]
-    print(f'Getting VM metrics for job {ind}...')
-    if(ind<5 or ind==7):
-      vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(
-        start_time_sec, end_time_sec, INSTANCE, PERIOD, 'ReadFile')
-    else:
-      vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(
-        start_time_sec, end_time_sec, INSTANCE, PERIOD, 'WriteFile')
+    rw = 'ReadFile'
+    if job[RW] == 'read' or job[RW] == 'randread':
+      rw = 'ReadFile'
+    elif job[RW] == 'write' or job[RW] == 'randwrite':
+      rw = 'WriteFile'
+
+    print(f'Getting VM metrics for job {ind+1}...')
+    vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(
+        start_time_sec, end_time_sec, INSTANCE, PERIOD, rw)
