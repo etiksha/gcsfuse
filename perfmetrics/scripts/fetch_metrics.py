f986f044bbdae36f515a2d68e3cac40af415add5
etiksha <107264895+etiksha@users.noreply.github.com>
2022-07-01 14:36:16 +0530

    Update fetch_metrics.py

--- perfmetrics/scripts/fetch_metrics.py
+++ perfmetrics/scripts/fetch_metrics.py
@@ -4,7 +4,7 @@ import socket
 import sys
 import time
 from fio import fio_metrics
-from vm_metrics import vm_metrics
+from vm_metrics import vmfinal1
 
 START_TIME = 'start_time'
 END_TIME = 'end_time'
@@ -26,19 +26,14 @@ if __name__ == '__main__':
   print('Waiting for 250 seconds for metrics to be updated on VM...')
   # It takes up to 240 seconds for sampled data to be visible on the VM metrics graph
   # So, waiting for 250 seconds to ensure the returned metrics are not empty
-  time.sleep(250)
+  #time.sleep(250)
 
-  vm_metrics_obj = vm_metrics.VmMetrics()
+  vm_metrics_obj = vmfinal1.VmMetrics()
   # Getting VM metrics for every job
   for ind, job in enumerate(temp):
     start_time_sec = job[START_TIME]
     end_time_sec = job[END_TIME]
-    rw = 'ReadFile'
-    if job[RW] == 'read' or job[RW] == 'randread':
-      rw = 'ReadFile'
-    elif job[RW] == 'write' or job[RW] == 'randwrite':
-      rw = 'WriteFile'
-
-    print(f'Getting VM metrics for job {ind+1}...')
+    rw = job[RW]
+    print(f'Getting VM metrics for job at index {ind+1}...')
     vm_metrics_obj.fetch_metrics_and_write_to_google_sheet(
-        start_time_sec, end_time_sec, INSTANCE, PERIOD, rw)
+        start_time_sec, end_time_sec, "etiksha-vm-metrics", PERIOD, rw)
